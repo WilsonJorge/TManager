9799138b40a4601038e2759fc76a63727faaea53
Wilson Macuacua <macuacuaw330@gmail.com>
2023-10-29 20:44:09 +0200

    creating userController

--- TManeger/src/controller/UserController.ts
+++ TManeger/src/controller/UserController.ts
@@ -14,16 +14,4 @@ class UserController {
     }
   }
 
-  async createUser(req: Request, res: Response) {
-    try {
-      const userRepository = getRepository(User);
-      const newUser = userRepository.create(req.body);
-      await userRepository.save(newUser);
-      return res.status(201).json(newUser);
-    } catch (error) {
-      return res.status(500).json({ error: 'Falha ao criar um user' });
-    }
-  }
-}
-
-export default new UserController();
+  
