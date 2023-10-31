43bc9a4684c7676b910a951b1b175e617679a116
Wilson Macuacua <macuacuaw330@gmail.com>
2023-10-31 08:58:00 +0200

    refactor

--- TManeger/src/controller/UserController.ts
+++ TManeger/src/controller/UserController.ts
@@ -3,15 +3,6 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
 
-class UserController {
-  async getAll(req: Request, res: Response) {
-    try {
-      const userRepository = getRepository(User);
-      const users = await userRepository.find();
-      return res.json(users);
-    } catch (error) {
-      return res.status(500).json({ error: 'Falha ao buscar users' });
-    }
-  }
+
 
   
