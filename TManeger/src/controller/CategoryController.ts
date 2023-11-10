ff0a7687da37386bbb70e4215d6dc04ce245fed1
Wilson Macuacua <macuacuaw330@gmail.com>
2023-11-10 21:33:20 +0200

    refactor: testing API

--- TManeger/src/controller/CategoryController.ts
+++ TManeger/src/controller/CategoryController.ts
@@ -32,6 +32,47 @@ class CategoryController {
       return res.status(500).json({ error: 'Falha ao criar uma categoria,Tente Novamente', reason: error.message });
     }
   }
+
+  async updateCategory(req: Request, res: Response) {
+    try {
+      console.log('Corpo da solicitação:', req); // Adicione esta linha para registrar o corpo da solicitação
+      const categoryRepository = getRepository(Category);
+      const {id, categoria, numeroCategoria} = req.body
+      const category = await categoryRepository.findOne({id});
+      if (!category) {
+        return res.status(404).json({ error: 'Categoria não encontrada' });
+      }
+      console.log('Categoria a ser atualizada:', category); // Adicione esta linha para registrar a categoria a ser atualizada
+      category.categoria = categoria;
+      category.numeroCategoria = numeroCategoria;
+      await categoryRepository.save(category);
+      console.log('Categoria atualizada com sucesso:', category); // Adicione esta linha para registrar a categoria atualizada
+      return res.status(200).json(category);
+    } catch (error) {
+      console.error('Erro ao atualizar uma categoria:', error);
+      return res.status(500).json({ error: 'Falha ao atualizar uma categoria,Tente Novamente', reason: error.message });
+    }
+  }
+   
+  async deleteCategory(req: Request, res: Response) {
+    try {
+      console.log('Corpo da solicitação:', req); // Adicione esta linha para registrar o corpo da solicitação
+      const categoryRepository = getRepository(Category);
+      const {id} = req.body
+      const category = await categoryRepository.findOne({id});
+      if (!category) {
+        return res.status(404).json({ error: 'Categoria não encontrada' });
+      }
+      console.log('Categoria a ser excluída:', category); // Adicione esta linha para registrar a categoria a ser excluída
+      await categoryRepository.remove(category);
+      console.log('Categoria excluída com sucesso:', category); // Adicione esta linha para registrar a categoria excluída
+      return res.status(200).json(category);
+    } catch (error) {
+      console.error('Erro ao excluir uma categoria:', error);
+      return res.status(500).json({ error: 'Falha ao excluir uma categoria,Tente Novamente', reason: error.message });
+    }
+  }
+
 }
 
 export default new CategoryController();
