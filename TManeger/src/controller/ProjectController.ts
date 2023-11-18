0266b50260986902e6a88024c034c6d19def4b79
Wilson Macuacua <macuacuaw330@gmail.com>
2023-11-18 13:35:24 +0200

    refactor:update Project Controller

--- TManeger/src/controller/ProjectController.ts
+++ TManeger/src/controller/ProjectController.ts
@@ -2,64 +2,106 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { Project } from '../entity/Project';
 
-export class ProjectController {
-  async getAllProjects(req: Request, res: Response) {
+class ProjectController {
+  async getAll(req: Request, res: Response) {
     try {
       const projectRepository = getRepository(Project);
       const projects = await projectRepository.find();
       return res.json(projects);
     } catch (error) {
-      console.error('Erro ao buscar projetos:', error);
-      return res.status(500).json({ error: 'Falha ao buscar projetos' });
+      console.error('Erro ao buscar Projecto:', error);
+      return res.status(500).json({ error: 'Falha ao buscar Projectos' });
     }
   }
 
   async createProject(req: Request, res: Response) {
     try {
+      console.log('Corpo da solicitação:', req); // Adicione esta linha para registrar o corpo da solicitação
       const projectRepository = getRepository(Project);
       const newProject = projectRepository.create(req.body);
+      console.log('Novo projecto a ser criado:', newProject); // Adicione esta linha para registrar a nova categoria
       await projectRepository.save(newProject);
+      console.log('Projecto criado com sucesso:', newProject); // Adicione esta linha para registrar a categoria criada
       return res.status(201).json(newProject);
     } catch (error) {
-      console.error('Erro ao criar um projeto:', error);
-      return res.status(500).json({ error: 'Falha ao criar um projeto', reason: error.message });
+      console.error('Erro ao criar um Porjecto:', error);
+      return res.status(500).json({ error: 'Falha ao criar um Projecto,Tente Novamente', reason: error.message });
     }
   }
 
   async updateProject(req: Request, res: Response) {
     try {
       const projectRepository = getRepository(Project);
-      const projectId = req.params.id;
-      const existingProject = await projectRepository.findOne(projectId);
-
-      if (!existingProject) {
-        return res.status(404).json({ error: 'Projeto não encontrado' });
+      const projectId: number = +req.params.id; // Certifique-se de que categoryId é do tipo correto (neste caso, número).
+      // Verifique se o ID da categoria foi fornecido
+      if (!projectId) {
+        return res.status(400).json({ error: 'ID do Projecto não fornecido' });
       }
-
-      projectRepository.merge(existingProject, req.body);
-      const updatedProject = await projectRepository.save(existingProject);
-      return res.json(updatedProject);
+  
+      // Busque a categoria no banco de dados
+      const existingProject = await projectRepository.findOne({
+        where: { id: projectId }
+      });
+      // Verifique se a categoria existe
+      if (!existingProject) {
+        return res.status(404).json({ error: 'Projecto não encontrada' });
+      }
+  
+      // Atualize o projecto com os novos dados do corpo da solicitação
+      const { projectName, startDate,  endDate,  projectCategory, country, telephone, company, companyPhone, city, Andress, tasks } = req.body;
+      existingProject.projectName = projectName;
+      existingProject.startDate = startDate;
+      existingProject.endDate=endDate;
+      existingProject.projectCategory= projectCategory;
+      existingProject.country = country;
+      existingProject.telephone= telephone;
+      existingProject.company=company;
+      existingProject.companyPhone=companyPhone;
+      existingProject.city= city;
+      existingProject.Andress=Andress;
+      existingProject.tasks=tasks;
+  
+      // Salve as alterações no banco de dados
+      await projectRepository.save(existingProject);
+  
+      return res.json(existingProject);
     } catch (error) {
-      console.error('Erro ao atualizar um projeto:', error);
-      return res.status(500).json({ error: 'Falha ao atualizar um projeto', reason: error.message });
+      console.error('Erro ao atualizar projecto:', error);
+      return res.status(500).json({ error: 'Falha ao atualizar projecto', reason: error.message });
     }
   }
-
   async deleteProject(req: Request, res: Response) {
     try {
       const projectRepository = getRepository(Project);
       const projectId = req.params.id;
-      const existingProject = await projectRepository.findOne(projectId);
-
-      if (!existingProject) {
-        return res.status(404).json({ error: 'Projeto não encontrado' });
+  
+      // Verifique se o ID da categoria foi fornecido
+      if (!projectId) {
+        return res.status(400).json({ error: 'ID do Projecto não fornecido' });
       }
+  
+      // Busque a categoria no banco de dados
+      const existingProject = await projectRepository.findOne({
+        where: { id: parseInt(projectId, 10) }
+      });
 
+      // Verifique se a categoria existe
+      if (!existingProject) {
+        return res.status(404).json({ error: 'Projecto não encontrado' });
+      }
+  
+      // Remova a categoria do banco de dados
       await projectRepository.remove(existingProject);
-      return res.json({ message: 'Projeto removido com sucesso' });
+  
+      return res.json({ message: 'Projecto removido com sucesso' });
     } catch (error) {
-      console.error('Erro ao excluir um projeto:', error);
-      return res.status(500).json({ error: 'Falha ao excluir um projeto', reason: error.message });
+      console.error('Erro ao excluir projecto:', error);
+      return res.status(500).json({ error: 'Falha ao excluir projecto', reason: error.message });
     }
   }
+  
+
 }
+
+export default new ProjectController();
+
